1d4926842b80756b33c1fd017bcb134d958ffb92
Purnachandra <pratap.careers@gmail.com>
2024-09-23 17:46:23 +0530

    changed url to production

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App.jsx';
 import './index.css';
 
 // global variables
-window.BACKEND_URL = 'http://localhost:3000';
-// window.BACKEND_URL = 'https://codewithpratap-backend-production.up.railway.app';
+// window.BACKEND_URL = 'http://localhost:3000';
+window.BACKEND_URL = 'https://codewithpratap-backend-production.up.railway.app';
 
 createRoot(document.getElementById('root')).render(
   <App />
